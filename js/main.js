a9f4a8a2a440fd5c6049f7869dac63291da145e2
Jeriah Bowers <jeriah97bowers@yahoo.com>
2018-12-31 12:24:52 -0500

    finishing up home page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ $(window).on("scroll", () => {
         $(".header__nav").removeClass("active")
     }
 });
+
+
+// Adding slide up effect on blog divs
+
+// first 
+$(".first").on("mouseover", () => {
+    $(".firstSlide").slideDown(200);
+});
+$(".first").on("mouseout", (evt) => {
+    $(".firstSlide").slideUp(200);
+});
+
+// second
+$(".second").on("mouseover", () => {
+    $(".secondSlide").slideDown(200);
+});
+$(".second").on("mouseout", (evt) => {
+    $(".secondSlide").slideUp(200);
+});
+
+// third
+$(".third").on("mouseover", () => {
+    $(".thirdSlide").slideDown(200);
+});
+$(".third").on("mouseout", (evt) => {
+    $(".thirdSlide").slideUp(200);
+});
+
